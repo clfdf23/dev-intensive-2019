575216d1aa983fa8dfccede1efc82d57a7326c8a
clfdf <clfdf23@gmail.com>
2019-07-30 14:04:56 +0300

    after lection 47 min

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,30 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Bender
+
+class ProfileActivity : AppCompatActivity() {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+    }
+
+
+}
